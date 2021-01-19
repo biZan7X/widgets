6e73ab82b2f696b607abe452b5509cd252aabe50
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-01-19 10:52:41 +0530

    feat - navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,13 @@ import react from "react";
 
 const Link = ({ className, href, children }) => {
 	const onClick = (event) => {
+		if (event.metaKey || event.ctrlKey) return; //* if ctrl is on , just do the natural thing
+
 		event.preventDefault();
+		window.history.pushState({}, "", href);
+
+		const navEvent = new PopStateEvent("popstate"); //^ creating the event
+		window.dispatchEvent(navEvent); //^exporting the event
 	};
 	return (
 		<a onClick={onClick} className={className} href={href}>
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+
 const React = ({ path, children }) => {
-	return window.location.pathname === path ? children : null;
+	const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+	useEffect(() => {
+		const onLocationChange = () => {
+			setCurrentPath(window.location.pathname);
+		};
+
+		window.addEventListener("popstate", onLocationChange);
+	}, []);
+
+	//^when the state gets changed it forces the component to re render and hence doing the checks again.
+	return currentPath === path ? children : null;
 };
 
 export default React;
